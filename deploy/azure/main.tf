c3b2e029779b6bbcff2ae483f2a5149c22c8efdd
Petar Gjeorgiev <petar@blameless.com>
2021-11-02 15:50:44 +0100

    Include nat gateway and add Enable Route All

--- deploy/azure/main.tf
+++ deploy/azure/main.tf
@@ -62,14 +62,14 @@ module "apiManagement" {
   apimanagement_name                = var.apimanagement_name
 }
 
-# module "natGateway" {
-#   source                 = "./natGateway"
-#   location               = var.location
-#   resource_group_name    = module.resourceGroup.resource_group_name#var.resource_group_name
-#   natGateway_name        = "nat-${var.functionapp_name}"
-#   vnet_name              = "vnet-${var.functionapp_name}"
-#   subnet_name            = "subnet-${var.functionapp_name}"
-#   public_ip_name         = "ip-${var.functionapp_name}"
-#   subnet_delegation_name = "snetdel-${var.functionapp_name}"
-#   app_service_id         = module.function.functionapp_id
-# }
+module "natGateway" {
+  source                 = "./natGateway"
+  location               = var.location
+  resource_group_name    = module.resourceGroup.resource_group_name#var.resource_group_name
+  natGateway_name        = "nat-${var.functionapp_name}"
+  vnet_name              = "vnet-${var.functionapp_name}"
+  subnet_name            = "subnet-${var.functionapp_name}"
+  public_ip_name         = "ip-${var.functionapp_name}"
+  subnet_delegation_name = "snetdel-${var.functionapp_name}"
+  app_service_id         = module.function.functionapp_id
+}
