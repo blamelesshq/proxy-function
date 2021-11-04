5eb03b04afcf053548b5f9455e838793a88d0dbc
Petar Gjeorgiev <petar@blameless.com>
2021-11-04 18:49:51 +0100

    Modify command

--- deploy/azure/functionDeploy/main.tf
+++ deploy/azure/functionDeploy/main.tf
@@ -2,6 +2,6 @@ data "azurerm_client_config" "current" {}
 
 resource "null_resource" "example1" {
   provisioner "local-exec" {
-    command = "sleep 15; cd ../../Splunk; go build main.go; func azure functionapp publish ${var.azure_func_name}"
+    command = "sleep 15; cd ../../Splunk; go build ./main.go; func azure functionapp publish ${var.azure_func_name}"
   }
 }
