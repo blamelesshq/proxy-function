6f70f16bdc43ba124bc5929ed68322c620ec3581
Petar Gjeorgiev <petar@blameless.com>
2021-11-10 13:25:11 +0100

    Add blameless code builder

---
+++ Splunk/CodeBuilder/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using BlamelessCodeBuilder;
+using YamlDotNet.Serialization.NamingConventions;
+
+var yamlDeserializer = new YamlDotNet.Serialization.DeserializerBuilder()
+    .WithNamingConvention(CamelCaseNamingConvention.Instance)
+    .Build();
+
+var myConfig = yamlDeserializer.Deserialize<RouteConfig>(File.ReadAllText("route-config.yaml"));
+
+var myConfigJsonFormat = JsonConvert.SerializeObject(myConfig);
+myConfigJsonFormat = JsonConvert.ToString(myConfigJsonFormat);
+myConfigJsonFormat = JsonConvert.ToString(myConfigJsonFormat);
+
+var functionDirectoriesList = new List<string>();
+myConfig.Functions.ForEach(f =>
+{
+    var functionName = f.Route.Split('/')[^1];
+    var functionDirExists = Directory.Exists(functionName);
+    if (!functionDirExists)
+    {
+        functionDirectoriesList.Add(functionName);
+        var createFunctionDirResult = Shell.Bash($"func new --name {functionName} --template \"HTTP trigger\" --authlevel \"anonymous\" --methods \"get\"", true);
+    }
+});
+
+var updateKeyVaultResult = Shell.Bash($"az keyvault secret set --vault-name \"{args[0]}\" --name \"RouteConfig\" --value \"{myConfigJsonFormat}\"");
+Console.WriteLine(updateKeyVaultResult);
+
+var buildMainResult = Shell.Bash($"go build ./main.go");
+Console.WriteLine(buildMainResult);
+
+var functionPublishResult = Shell.Bash($"func azure functionapp publish {args[1]} --custom");
+Console.WriteLine(functionPublishResult);
+
+functionDirectoriesList.ForEach(newFunctionDirectory =>
+{
+    Directory.Delete(newFunctionDirectory, true);
+});
---
+++ Splunk/CodeBuilder/RouteConfig.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace BlamelessCodeBuilder
+{
+    public class Function
+    {
+        public string Route { get; set; } = default!;
+        public string Url { get; set; } = default!;
+        public string AccessToken { get; set; } = default!;
+        public string Type { get; set; } = default!;
+        public string Login { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+
+    public class RouteConfig
+    {
+        public List<Function> Functions { get; set; } = default!;
+    }
+
+    public static class Shell
+    {
+        public static string Bash(this string cmd, bool isEscaped = false)
+        {
+            var escapedArgs = isEscaped ? cmd.Replace("\"", "\\\"") : cmd;
+            string? fileName;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                fileName = "/System/Applications/Utilities/Terminal.app";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                fileName = "/bin/sh";
+            }
+            else
+            {
+                fileName = @"C:\Program Files\Git\bin\sh";
+            }
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = fileName,
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            return result;
+        }
+    }
+}
