a44c2c20fc8a7d39c6e1ee7674ca32488e1c727c
Petar Gjeorgiev <petar@blameless.com>
2021-11-15 17:21:41 +0100

    Modify program.cs

--- Splunk/CodeBuilder/Program.cs
+++ Splunk/CodeBuilder/Program.cs
@@ -7,35 +7,52 @@ var yamlDeserializer = new YamlDotNet.Serialization.DeserializerBuilder()
 
 var myConfig = yamlDeserializer.Deserialize<RouteConfig>(File.ReadAllText("route-config.yaml"));
 
-var myConfigJsonFormat = System.Text.Json.JsonSerializer.Serialize(myConfig);
 
-myConfigJsonFormat = $"\"{myConfigJsonFormat.Replace("\"", "\\\"")}\"";
-myConfigJsonFormat = myConfigJsonFormat.Replace("\"", "\\\\\"");
-myConfigJsonFormat = $"\"{myConfigJsonFormat.Substring(1, myConfigJsonFormat.Length - 3)}\"{myConfigJsonFormat[myConfigJsonFormat.Length - 1]}";
-
-
-var functionDirectoriesList = new List<string>();
-myConfig.Functions.ForEach(f =>
+try
 {
-    var functionName = f.Route.Split('/')[^1];
-    var functionDirExists = Directory.Exists(functionName);
-    if (!functionDirExists)
+    var myConfigJsonFormat = System.Text.Json.JsonSerializer.Serialize(myConfig);
+
+    Console.WriteLine("Before escape: " + myConfigJsonFormat);
+    
+
+    myConfigJsonFormat = $"\"{myConfigJsonFormat.Replace("\"", "\\\"")}\"";
+    Console.WriteLine("After escape 1: " + myConfigJsonFormat);
+    myConfigJsonFormat = myConfigJsonFormat.Replace("\"", "\\\\\"");
+    myConfigJsonFormat = $"\"{myConfigJsonFormat.Substring(1, myConfigJsonFormat.Length - 3)}\"{myConfigJsonFormat[myConfigJsonFormat.Length - 1]}";
+    Console.WriteLine("After escape 2: " + myConfigJsonFormat);
+
+
+    var functionDirectoriesList = new List<string>();
+    myConfig.Functions.ForEach(f =>
     {
-        functionDirectoriesList.Add(functionName);
-        var createFunctionDirResult = Shell.Bash($"func new --name {functionName} --template \"HTTP trigger\" --authlevel \"anonymous\" --methods \"get\"", true);
-    }
-});
+        var functionName = f.Route.Split('/')[^1];
+        var functionDirExists = Directory.Exists(functionName);
+        if (!functionDirExists)
+        {
+            functionDirectoriesList.Add(functionName);
+            var createFunctionDirResult = Shell.Bash($"func new --name {functionName} --template \"HTTP trigger\" --authlevel \"anonymous\" --methods \"get\"", true);
+        }
+    });
 
-var updateKeyVaultResult = Shell.Bash($"az keyvault secret set --vault-name \"{args[0]}\" --name \"RouteConfig\" --value \"{myConfigJsonFormat}\"");
-Console.WriteLine(updateKeyVaultResult);
+    Console.WriteLine("Before key vault");
+    Console.WriteLine("Args: " + args.Length);
+    var updateKeyVaultResult = Shell.Bash($"az keyvault secret set --vault-name \"{args[0]}\" --name \"RouteConfig\" --value \"{myConfigJsonFormat}\"");
+    Console.WriteLine("After key vault");
+    Console.WriteLine(updateKeyVaultResult);
 
-var buildMainResult = Shell.Bash($"go build ./main.go");
-Console.WriteLine(buildMainResult);
+    var buildMainResult = Shell.Bash($"go build ./main.go");
+    Console.WriteLine(buildMainResult);
 
-var functionPublishResult = Shell.Bash($"func azure functionapp publish {args[1]} --custom");
-Console.WriteLine(functionPublishResult);
+    var functionPublishResult = Shell.Bash($"func azure functionapp publish {args[1]} --custom");
+    Console.WriteLine(functionPublishResult);
 
-functionDirectoriesList.ForEach(newFunctionDirectory =>
+    functionDirectoriesList.ForEach(newFunctionDirectory =>
+    {
+        Directory.Delete(newFunctionDirectory, true);
+    });
+}
+catch (Exception e)
 {
-    Directory.Delete(newFunctionDirectory, true);
-});
+    Console.WriteLine(e.Message);
+    Console.WriteLine(e.StackTrace);
+}
