c0f1917ecfe66941d45b016651f3e04506c5841b
Sarah Collison <sarah@blameless.com>
2022-05-31 11:15:07 -0700

    Formatting

--- deploy/gcp/terraform.tf
+++ deploy/gcp/terraform.tf
@@ -4,7 +4,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.18"
+      version = "~> 4.22"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.22"
     }
   }
 }
